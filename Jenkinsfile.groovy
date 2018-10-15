d86ee8a5d1e95d8b281c118d653aeeea2536e894
naveen <naveen91.ganji@gmail.com>
2018-10-14 21:46:38 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,22 +2,22 @@ pipeline {
 	agent any
 	
 	stages {
-		stage('Clone repository') {
+		stage('stage1') {
         /* Let's make sure we have the repository cloned to our workspace */
         //checkout scm
         }
              
-        stage("Install AWSCLI") {
-			steps {
+        stage("stage2") {
+			//steps {
 				//sh 'sudo apt-get install python3'
                 //sh 'sudo pip install awscli'
-			}
+			//}
 		}
 		
-        stage("Deploy") {
-			steps {
+        stage("stage3") {
+			//steps {
 				//sh 'aws apigateway import-rest-api --parameters ignore=documentation endpointConfigurationTypes=EDGE --body 'file:///swagger-test-api.json'
-			}
+			//}
 		}
     }
 }
